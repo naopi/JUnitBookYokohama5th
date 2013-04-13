944004cce12ed449edf50e53e7638d5c3665e923
Naoya ISHII <naopi.chan@gmail.com>
2013-04-13 23:43:08 +0900

    P.338中段まで

---
+++ src/test/java/cucumber/poker/PokerGameAcceptanceTest.java
@@ -0,0 +1,11 @@
+package cucumber.poker;
+
+import cucumber.junit.Cucumber;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@Cucumber.Options(format = {"pretty"}, monochrome = true)
+public class PokerGameAcceptanceTest
+{
+
+}
